92a96bbf48012f89881815d6beb060d57398ae7f
danilshatalin <130211552+danilshatalin@users.noreply.github.com>
2023-06-10 17:11:41 +0400

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,7 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -6,62 +10,34 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        //System.out.printf(Integer.toString(factorial(5)));
-        //printPrimeNumber(1000);
-        calc();
-    }
-    public static int triangle(int n){
-        return (n * (n + 1)/2);
-    }
-    public static int factorial(int n){
-        if(n == 1){
-            return 1;
-        }
-        return n * factorial(n - 1);
-    }
-    public static boolean isPrime(int n){
-        int d = 2;
-        while (((d * d) <= n) && (n % d != 0)){
-            d += 1;
-        }
-        return d * d > n;
-    }
-    public static void printPrimeNumber(int n){
-        for( int i = 2; i <= n; i++){
-            if(isPrime(i) == true){
-                System.out.print(Integer.toString(i) + ' ');
-            }
-        }
-    }
-    public static void calc(){
-        int a,b;
-        String c = "";
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите 1-е число\n");
-        a = scan.nextInt();
-        System.out.print("Введите 2-е число\n");
-        b = scan.nextInt();
-        System.out.print("Введите операцию из списка *, /, +, -\n");
-        c = scan.next();
-        switch (c){
-            case "/":{
-                System.out.print(Double.toString((double) a/(double) b));
-                return;
-            }
-            case "+":{
-                System.out.print(Integer.toString(a+b));
-                return;
-            }
-            case "-":{
-                System.out.print(Integer.toString(a-b));
-                return;
-            }
-            case  "*":{
-                System.out.print(Integer.toString(a*b));
-                return;
-            }
-        }
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("outdata.txt");
+            FileReader fr = null;
+            fr = new FileReader("jsondata.txt");
+            Scanner scan = new Scanner(fr);
 
-
+            String jsonstr =  scan.nextLine().replace('{','}');
+            jsonstr = jsonstr.substring(1,jsonstr.length() - 2);
+            String[] jsonObjects = jsonstr.split("}");
+            String[] fildObj;
+            StringBuilder outStr = new StringBuilder();
+            for (int i = 1; i < jsonObjects.length; i+= 2) {
+                fildObj = jsonObjects[i].strip().split("\"");
+                outStr.append("Студент ");
+                outStr.append(fildObj[3]);
+                outStr.append(" получил(а) ");
+                outStr.append(fildObj[7]);
+                outStr.append(" по предмету ");
+                outStr.append(fildObj[11]);
+                System.out.println(outStr);
+                outStr.append("\n");
+                fw.write(outStr.toString());
+                outStr = new StringBuilder();
+            }
+            fw.close();
+        } catch (IOException e) {;
+            throw new RuntimeException(e);
+        }
     }
 }
